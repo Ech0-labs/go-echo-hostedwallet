92a43ea0ca576154e0d9e692ed4d36825bfc9aa4
nem0z <maxime.bernard.dev@gmail.com>
2023-11-17 17:07:41 +0100

    feat (main): calc minRelayFees before sending the tx

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ import (
 	"github.com/ltcsuite/ltcd/rpcclient"
 )
 
-const Message = "Elon musk, get ready for ratio"
+const message = "Message to send throw the blockchain"
 const userAddr = "ltc1qxwlk7kh2vx8xgvff0rxgrtzh2v00lzp7v9updr"
 const echoAddr = "ltc1qwewx5q6fwy6d0acuu3mk29cema4hkhew5afmsa"
 const dust = 2940 / ltcutil.SatoshiPerBitcoin
-const fees = 182 / ltcutil.SatoshiPerBitcoin
+const fees = 1 / ltcutil.SatoshiPerBitcoin
 
 func Handle(err error) {
 	if err != nil {
@@ -44,15 +44,6 @@ func main() {
 	Handle(err)
 	defer client.Shutdown()
 
-	blockCount, err := client.GetBlockCount()
-	Handle(err)
-
-	fmt.Println("Block height :", blockCount)
-
-	if true {
-		//return
-	}
-
 	utxo, err := client.ListUnspent()
 	userUTXO := utils.FilterUserAddrUTXO(userAddr, utxo)
 
@@ -66,9 +57,16 @@ func main() {
 
 	outputs := tx.CreateOutputs(echoOutput, restOutput)
 
-	message := []byte(Message)
+	message := []byte(message)
 
-	hash, err := tx.Send(*client, inputs, outputs, message)
+	minRelayFees, err := tx.MinRelayFees(client, inputs, outputs, message)
+	Handle(err)
+
+	restOutput, err = tx.CreateOutput(userAddr, amount-dust-minRelayFees)
+	Handle(err)
+	outputs = tx.CreateOutputs(echoOutput, restOutput)
+
+	hash, err := tx.Send(client, inputs, outputs, message)
 	Handle(err)
 
 	fmt.Println("hash of the tx :", hash)
